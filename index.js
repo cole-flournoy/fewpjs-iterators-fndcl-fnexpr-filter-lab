b3d6634669c2afb34ca6eda350ed4bad0effa7d7
Cole Flournoy <flournoy.cole@gmail.com>
2021-04-22 15:08:20 -0400

    commit tree 1a9094a96b85a9dd33827d2a3ab8ea53e8737528

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, name) {  
+  return drivers.filter(d => d.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters) {
+  const len = letters.length
+  return drivers.filter(d => d.slice(0, len) === letters)
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(d => d.name.toLowerCase() === name.toLowerCase()) 
+}
